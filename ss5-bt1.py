b69afcff37caaa5823f47699a5635d564ea4e3bc
poldayy <huynhhonhidan@gmail.com>
2026-05-29 14:28:08 +0700

    Update ss5-bt1.py

--- ss5-bt1.py
+++ ss5-bt1.py
@@ -5,12 +5,12 @@ month_count = 3
 
 result = ""
 
-for month in range(1, month_count + 1):
-    for branch in range(1, branch_count + 1):
+for branch in range(1, branch_count + 1):
+    for month in range(1, month_count + 1):
         revenue = int(
-            input(f"Nhập doanh thu Chi nhánh {month}, tháng {branch}: ")
+            input(f"Nhập doanh thu Chi nhánh {branch}, tháng {month}: ")
         )
 
-        result = result + f"Chi nhánh {month}, tháng {branch}: {revenue} triệu đồng\n"
+        result = result + f"Chi nhánh {branch}, tháng {month}: {revenue} triệu đồng\n"
 
-print(result)
+print(result) 
